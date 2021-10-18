95180063442313e3e1d7811a924bedbf3f948635
CarvalhoNathan <fariasnathan75@gmail.com>
2021-10-17 22:04:27 -0300

    Add Background animation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
 
 function handleKeyUp(event){
@@ -17,8 +19,8 @@ function jump(){
   let upInterval = setInterval(() => {
     if (position >= 150){
       clearInterval(upInterval);
-
       // Descendo
+      
       let downInterval = setInterval(() => {
         if (position <= 0){
           clearInterval(downInterval);
@@ -40,19 +42,22 @@ function createCactus(){
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
   let randomTime = Math.random() * 6000;
+
+  if (isGameOver) return;
   
   cactus.classList.add('cactus');
-  cactus.style.left = 1000 + 'px';
   background.appendChild(cactus);
+  cactus.style.left = 1000 + 'px';
 
-  let leftInterval = setInterval(() => {
+  let leftInterval= setInterval(() => {
     if(cactusPosition < - 60){
+      // Saiu da tela
       clearInterval(leftInterval);
       background.removeChild(cactus);
     } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
       // Game Over
       clearInterval(leftInterval);
-      document.body.innerHTML = <h1 class="gamer-over">Fim de jogo</h1>;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
